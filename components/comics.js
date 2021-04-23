cf0445660fe44eab6ce81acaa7ca04bee2994d7d
superpacmanchef <52766591+superpacmanchef@users.noreply.github.com>
2021-04-23 02:41:53 +0100

    Minor Changes

--- components/comics.js
+++ components/comics.js
@@ -1,19 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
-import { StyleSheet, View, Image, ImageBackground } from 'react-native';
-import { Dimensions } from 'react-native';
-import LogedContext from '../contexts/logedContext';
-import { comicTitleSplit } from '../utils/comicDataProcessesing';
-import { PullContext } from '../contexts/pullContext';
-import { getPull } from '../apis/UserDatabaseApi';
+import React, { useContext, useEffect, useState } from "react";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { StyleSheet, View, Image, ImageBackground } from "react-native";
+import { Dimensions } from "react-native";
+import LogedContext from "../contexts/logedContext";
+import { comicTitleSplit } from "../utils/comicDataProcessesing";
+import { PullContext } from "../contexts/pullContext";
+import { getPull } from "../apis/UserDatabaseApi";
 
 const Comics = (props) => {
   const { comics, comicsPressHandler } = props;
 
-  const [loged, updateLoged] = useContext(LogedContext);
+  const [loged] = useContext(LogedContext);
   const [pulledArr, updatePulledArr] = useState([]);
   const [pull, updatePull] = useContext(PullContext);
 
+  //On comics load check - user logged in - if comics are on pull list
   useEffect(() => {
     if (loged) {
       if (pull == false) {
@@ -26,6 +27,8 @@ const Comics = (props) => {
     }
   }, [comics, pull]);
 
+  //Check if comics are on pull list
+  //Returns T/F array
   const checkPull = () => {
     const arr = [];
     if (loged && comics) {
@@ -37,7 +40,7 @@ const Comics = (props) => {
           for (let x = 0; x < pull.length; x++) {
             let [name] = comicTitleSplit(comic.title);
             if (name) {
-              name = name.replace(/AND /g, '');
+              name = name.replace(/AND /g, "");
               let strippedPull = pull[x];
               if (strippedPull === name) {
                 console.log(name);
@@ -65,14 +68,16 @@ const Comics = (props) => {
           <View style={styles.comic}>
             <TouchableOpacity
               activeOpacity={0.5}
-              onPress={() => comicsPressHandler(index)}>
+              onPress={() => comicsPressHandler(index)}
+            >
               <ImageBackground
                 style={styles.comicCover}
-                source={{ uri: item.image }}>
+                source={{ uri: item.image }}
+              >
                 {pulledArr[index] ? (
                   <Image
                     style={styles.star}
-                    source={require('../assets/dd.png')}
+                    source={require("../assets/dd.png")}
                   />
                 ) : (
                   <></>
@@ -86,17 +91,17 @@ const Comics = (props) => {
   );
 };
 
-const hieght = Dimensions.get('window').height;
+const hieght = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   comic: {
-    alignItems: 'center',
-    backgroundColor: 'rgb(0, 194, 128)',
+    alignItems: "center",
+    backgroundColor: "rgb(0, 194, 128)",
     borderRadius: 10,
-    marginHorizontal: '5%',
-    paddingVertical: '2%',
-    width: '45%',
-    marginLeft: '1.5%',
-    marginBottom: '5%',
+    marginHorizontal: "5%",
+    paddingVertical: "2%",
+    width: "45%",
+    marginLeft: "1.5%",
+    marginBottom: "5%",
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.5,
     shadowRadius: 1,
@@ -109,7 +114,7 @@ const styles = StyleSheet.create({
   star: {
     height: hieght * 0.03,
     aspectRatio: 1.1,
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start",
   },
 });
 
